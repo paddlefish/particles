ff28cacadc1e596670d174ce36c207223c8f14a6
Andrew Rahn <arahn@adobe.com>
2014-10-06 22:32:18 -0500

    Add gradient of EM force from center getting stronger towards the edge.
    This makes some pretty convincing circular paths for the particles.

--- particles.love/main.lua
+++ particles.love/main.lua
@@ -1,14 +1,16 @@
 
--- Example: Physics
--- Grabbity by Xcmd
--- Updated by Dresenpai
--- Updated 0.8.0 by Bartoleo
+-- Cyclotron by Andrew Rahn
+
 math.randomseed( os.time() )
+
+----------------------------------------------------------------------
+-- "globals"
+----------------------------------------------------------------------
+
 local needBall = 100
-
-local removeObject = function()
-
-end
+local myBallBodies = {}
+local log = {}
+local emForceConstant = 1
 
 local aparatus = {
 	{ 590, 90 },
@@ -22,33 +24,42 @@ local aparatus = {
 	{ 590, 90 },
 }
 
+----------------------------------------------------------------------
+
 -- Convert the points into rectangles defining the edge
 -- by connecting each to the next
-local prev = nil
-for _, wall in ipairs( aparatus ) do
-	if not wall[ 1 ] then
-		prev = nil
+do
+	local prev = nil
+	for _, wall in ipairs( aparatus ) do
+		if not wall[ 1 ] then
+			prev = nil
+		end
+		if prev then
+			wall[ 3 ], wall[ 4 ] = prev[ 1 ], prev[ 2 ]
+				-- Copy the x1, y1 coord of the prev edge onto this one
+				-- note that lua indexes arrays starting at 1
+				-- also isn't it cool that you can assign two things at once
+		end
+		prev = wall[ 1 ] and wall
 	end
-	if prev then
-		wall[ 3 ], wall[ 4 ] = prev[ 1 ], prev[ 2 ]
-			-- Copy the x1, y1 coord of the prev edge onto this one
-			-- note that lua indexes arrays starting at 1
-			-- also isn't it cool that you can assign two things at once
-	end
-	prev = wall[ 1 ] and wall
 end
 
-local myBallBodies = {}
-local log = {}
+----------------------------------------------------------------------
 
 local function randomValue( multiplier )
 	return ( math.random() - 0.5 ) * multiplier
 end
+
+----------------------------------------------------------------------
+
 local function randomSpeed( multiplier )
 	local angle = randomValue( math.pi / 2 ) + math.pi
 	local speed = randomValue( multiplier )
 	return speed * math.cos( angle ), speed * math.sin( angle )
 end
+
+----------------------------------------------------------------------
+
 local function addBall( x, y )
 	local myBallBody = love.physics.newBody( myWorld, x, y, "dynamic" )
 	myBallShape = love.physics.newCircleShape( 0, 0, 0.001 )
@@ -61,12 +72,40 @@ local function addBall( x, y )
 	log[ #log + 1 ] = { "Added ball at %d, %d", x, y }
 end
 
+----------------------------------------------------------------------
+
+local function emForce( body )
+	local dx, dy = body:getLinearVelocity()
+	local x, y = body:getPosition()
+	x = 300 - x
+	y = 300 - y
+	local dist = emForceConstant * math.sqrt( x * x + y * y ) / 300
+--	local angle = math.atan2( dx, dy )
+--	angle = angle + math.pi / 2
+--	local x2, y2 = dist * math.cos( angle ), dist * math.sin( angle )
+	return dist *dy, dist * (-dx)
+end
+
+----------------------------------------------------------------------
+
+local function destroy( fixture )
+	local a = fixture:getBody()
+	if myBallBodies[ a ] then
+		log[ #log + 1 ] = { "Destroyed ball %s", tostring( a ) }
+		myBallBodies[ a ] = nil
+		a:destroy()
+		needBall = needBall + 1
+	end
+end
+
+----------------------------------------------------------------------
+
 function love.load()
 
 	love.graphics.setFont(love.graphics.newFont( 11))
 
 	love.physics.setMeter( 32 )
-	myWorld = love.physics.newWorld(0, 9.81*32, true)  -- updated Arguments for new variant of newWorld in 0.8.0
+	myWorld = love.physics.newWorld(0, 9.81*32, true)
 	gravity="none"
 	myWorld:setGravity(0, 0)
 	myWorld:setCallbacks( beginContact, endContact, preSolve, postSolve )
@@ -89,46 +128,33 @@ function love.load()
 
 end
 
-local function emForce( body )
-	local dx, dy = body:getLinearVelocity()
---	local dist = math.sqrt( dx * dx + dy * dy )
---	local angle = math.atan2( dx, dy )
---	angle = angle + math.pi / 2
---	local x2, y2 = dist * math.cos( angle ), dist * math.sin( angle )
-	return dy, -dx
-end
+----------------------------------------------------------------------
 
-local function destroy( fixture )
-	local a = fixture:getBody()
-	if myBallBodies[ a ] then
-		log[ #log + 1 ] = { "Destroyed ball %s", tostring( a ) }
-		myBallBodies[ a ] = nil
-		a:destroy()
-		needBall = needBall + 1
+do
+	local t = 0.1
+	function love.update( dt )
+		for myBallBody in pairs( myBallBodies ) do
+			myBallBody:applyForce( emForce( myBallBody ) )
+			local dx, dy = myBallBody:getLinearVelocity()
+			local speed = math.sqrt( dx * dx + dy * dy )
+			if speed < 50 then
+				destroy( myBallBody:getFixtureList()[ 1 ] )
+			end
+		end
+		myWorld:update( dt )
+		if t - dt < 0 then
+			if needBall > 0 then
+				addBall( 200, 110 )
+				needBall = needBall - 1
+			end
+			t = t + 0.1
+		else
+			t = t - dt
+		end
 	end
 end
 
-local t = 0.1
-function love.update( dt )
-    for myBallBody in pairs( myBallBodies ) do
-        myBallBody:applyForce( emForce( myBallBody ) )
-		local dx, dy = myBallBody:getLinearVelocity()
-		local speed = math.sqrt( dx * dx + dy * dy )
-		if speed < 50 then
-			destroy( myBallBody:getFixtureList()[ 1 ] )
-		end
-    end
-	myWorld:update( dt )
-	if t - dt < 0 then
-		if needBall > 0 then
-			addBall( 200, 110 )
-			needBall = needBall - 1
-		end
-		t = 0.1
-	else
-		t = t - dt
-	end
-end
+----------------------------------------------------------------------
 
 function love.draw()
 		love.graphics.setColor( 125, 125, 125 )
@@ -145,13 +171,13 @@ function love.draw()
 	end
 
 	love.graphics.setColor( 125, 125, 125 )
-	love.graphics.print( "gravity:"..gravity, 25, 25 )
-	if prepostsolve then
-	  love.graphics.print( "space : disable preSolve/postSolve Logging", 400, 25 )
-	else
-	  love.graphics.print( "space : enable preSolve/postSolve Logging", 400, 25 )
-	end
-	love.graphics.print( "arrows : change gravity direction", 400, 36 )
+	love.graphics.print( "EM Force Constant:"..emForceConstant, 25, 25 )
+--	if prepostsolve then
+--	  love.graphics.print( "space : disable preSolve/postSolve Logging", 400, 25 )
+--	else
+--	  love.graphics.print( "space : enable preSolve/postSolve Logging", 400, 25 )
+--	end
+	love.graphics.print( "up/down arrows : change EM force constant", 400, 36 )
 	for _, line in ipairs( log ) do
 		love.graphics.print( string.format( unpack( line, 1, line.n ) ), 400, 25 + _ * 25 )
 	end
@@ -161,31 +187,33 @@ function love.draw()
 
 end
 
+----------------------------------------------------------------------
+
 function love.keypressed( key )
    if key == "up" then
-      myWorld:setGravity(0, -9.81*32)
+      emForceConstant = emForceConstant + 0.1
       gravity="up"
       for i,v in ipairs(myWorld:getBodyList( )) do
         v:setAwake( true )
       end
    elseif key == "down" then
-      myWorld:setGravity(0, 9.81*32)
+      emForceConstant = emForceConstant - 0.1
       gravity="down"
       for i,v in ipairs(myWorld:getBodyList( )) do
         v:setAwake( true )
       end
-   elseif key == "left" then
-      myWorld:setGravity(-9.81*32, 0)
-      gravity="left"
-      for i,v in ipairs(myWorld:getBodyList( )) do
-        v:setAwake( true )
-      end
-  elseif key == "right" then
-      myWorld:setGravity(9.81*32, 0)
-      gravity="right"
-      for i,v in ipairs(myWorld:getBodyList( )) do
-        v:setAwake( true )
-      end
+--   elseif key == "left" then
+--      myWorld:setGravity(-9.81*32, 0)
+--      gravity="left"
+--      for i,v in ipairs(myWorld:getBodyList( )) do
+--        v:setAwake( true )
+--      end
+--  elseif key == "right" then
+--      myWorld:setGravity(9.81*32, 0)
+--      gravity="right"
+--      for i,v in ipairs(myWorld:getBodyList( )) do
+--        v:setAwake( true )
+--      end
    end
 
    if key == " " then
@@ -197,8 +225,12 @@ function love.keypressed( key )
    end
 end
 
+----------------------------------------------------------------------
+
 function beginContact( a, b, c )
 end
+
+----------------------------------------------------------------------
 
 function endContact( a, b, c )
    local aa = a:getUserData()
@@ -216,18 +248,17 @@ function endContact( a, b, c )
 	end
 end
 
+----------------------------------------------------------------------
+
 function preSolve( a, b, c )
 end
+
+----------------------------------------------------------------------
 
 function postSolve( a, b, c )
 end
 
-local function ifnil(ptest,preturn)
-   if p==nil then
-      return preturn
-   end
-   return ptest
-end
+----------------------------------------------------------------------
 
 function coll( a, b, c, ctype,detail )
 end
